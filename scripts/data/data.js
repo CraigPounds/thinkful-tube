44e6b91fd84d784ab2776382ce8c05f4b308e8eb
Craig Pounds <nweaver@windstream.net>
2018-04-26 16:07:58 -0500

    built data module

---
+++ scripts/data/data.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const  DATA = (function() {
+  let data = {
+    apiKey: 'AIzaSyA68gYnpN0ykW1n-V5ZmRZ0CyUR4EJA2zM',
+    endPoint: 'https://www.googleapis.com/youtube/v3/search',
+    videos: [],
+  };
+
+  const apiKey = data.apiKey;
+  const endPoint = data.endPoint;
+  const videos = data.videos;
+
+  return {
+    apiKey,
+    endPoint,
+    videos,
+  };
+}());

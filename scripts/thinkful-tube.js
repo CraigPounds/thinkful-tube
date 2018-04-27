353c3fd211e08d6b74964c5d06d65a19aae845f7
Craig Pounds <nweaver@windstream.net>
2018-04-26 22:05:50 -0500

    made thumbnails clickable, added styling to search button

--- scripts/thinkful-tube.js
+++ scripts/thinkful-tube.js
@@ -45,3 +45,9 @@ const THINKFUL_TUBE = (function() {
     handleSubmit,
   };
 }());
+
+/*
+  Make the images clickable, playing them in a lightbox
+  Show a link for more from the channel that each video came from
+  Show buttons to get more results (using the previous and next page links from the JSON)
+*/
--- scripts/utils/template.js
+++ scripts/utils/template.js
@@ -5,8 +5,8 @@ const TEMPLATE = (function() {
   function createThumbnails(video) {
     return `
       <li>
-      <h2>${video.title}</h2>
-      <a href="#"><img src="${video.thumbnail}" alt="${video.title}"></a>
+        <h2>${video.title}</h2>
+        <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img src="${video.thumbnail}" alt="${video.title}"></a>
       </li>
     `;
   }
